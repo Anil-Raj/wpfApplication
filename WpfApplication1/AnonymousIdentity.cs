c6c5a3f4c4bc44e18989f5c05afcf8a2a1366470
Anil G <anil.gangadharaiah@neudesic.com>
2017-07-01 22:17:39 +0530

    Added Authentication Service

---
+++ WpfApplication1/AnonymousIdentity.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfApplication1
+{
+    public class AnonymousIdentity : CustomIdentity
+    {
+        public AnonymousIdentity()
+            : base(string.Empty, string.Empty, new string[] { })
+        { }
+    }
+}
+
--- WpfApplication1/App.xaml.cs
+++ WpfApplication1/App.xaml.cs
@@ -13,5 +13,20 @@ namespace WpfApplication1
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            //Create a custom principal with an anonymous identity at startup
+            CustomPrincipal customPrincipal = new CustomPrincipal();
+            AppDomain.CurrentDomain.SetThreadPrincipal(customPrincipal);
+
+            base.OnStartup(e);
+
+            //Show the login view
+            AuthenticationViewModel viewModel = new AuthenticationViewModel(new AuthenticationService());
+            IView loginWindow = new LoginWindow(viewModel);
+            loginWindow.Show();
+
+        }
     }
 }
